1a5e4ef0ec703016e8b304c4a1113cdc52fcc2bb
harutiro <harutiro2027@outlook.jp>
2022-06-03 07:00:05 +0900

    add: welcomeActivityの作成

---
+++ Client/app/src/main/java/io/github/com/harutiro/tempmanager/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package io.github.com.harutiro.tempmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
