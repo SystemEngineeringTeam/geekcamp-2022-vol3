76a35e9de38f382b482a34d5b7d092bd79277b3c
harutiro <harutiro2027@outlook.jp>
2022-06-02 09:46:26 +0900

    remove: デフォルトでついているプログラムの消去

--- Client/app/src/main/java/io/github/com/harutiro/tempmanager/ui/home/HomeFragment.kt
+++ Client/app/src/main/java/io/github/com/harutiro/tempmanager/ui/home/HomeFragment.kt
@@ -22,16 +22,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+
         return root
     }
 
