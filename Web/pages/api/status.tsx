2b754216a62ab83fcc3e998f42f956b26b81b975
hihumikan <musecharo@gmail.com>
2022-06-04 05:52:04 +0900

    add: コメントの追加とフロントエンド側で表示を行う

--- Web/pages/api/status.tsx
+++ Web/pages/api/status.tsx
@@ -3,21 +3,25 @@ import { getFirestore } from 'firebase-admin/firestore';
 const admin = require('firebase-admin');
 
 
-// 日付(YYYY/MM/DD)を取得し、パースする(YYYY-MM-DD)
+// 日付(YYYY/MM/DD)を取得し、パースする関数(YYYY-MM-DD)
 function getDate() {
     const date = new Date().toLocaleDateString();
     const date_parse = date.split('/').join('-');
     return date_parse;
 }
+
+// ステータスを取得する関数
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
+    // コレクション指定
     const COLLECTION_NAME = 'RoomLog';
-    let users: string[] = [];
 
 
-    // 初期化を行う
+    // 初期化をする場合、最初の１回だけでいい
+    // lengthを取ると、初期化されているかどうかを確認できる
+    // https://github.com/firebase/firebase-tools/issues/1532
     if (admin.apps.length === 0) {
         admin.initializeApp({
             credential: admin.credential.cert({
--- Web/pages/index.tsx
+++ Web/pages/index.tsx
@@ -3,17 +3,19 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
-  const [getUsers, setUsers] = useState([]);
+  const [getUsers, setUsers] = useState();
   useEffect(() => {
     const getUser = async () => {
       const users = await axios.get('/api/status');
       console.log(users.data);
+      setUsers(users.data.name);
     };
     getUser();
   }, []);
 
   return (
     <div>
+      {getUsers}
     </div>
   );
 }
