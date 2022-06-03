a71336411df5476506ca17d553e9bf5dabb33a7e
harutiro <harutiro2027@outlook.jp>
2022-06-03 18:30:58 +0900

    upgrade: bindingの追加

--- Client/app/src/main/java/io/github/com/harutiro/tempmanager/welcome_ui/stater/StaterFragment.kt
+++ Client/app/src/main/java/io/github/com/harutiro/tempmanager/welcome_ui/stater/StaterFragment.kt
@@ -6,55 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.com.harutiro.tempmanager.R
+import io.github.com.harutiro.tempmanager.databinding.FragmentAcountBinding
+import io.github.com.harutiro.tempmanager.databinding.FragmentStaterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StaterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StaterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentStaterBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        binding = FragmentStaterBinding.inflate(layoutInflater)
+
+
         return inflater.inflate(R.layout.fragment_stater, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StaterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StaterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
