58ecdd7c85af4263ebd6844b0f1417ef31638a8f
harutiro <harutiro2027@outlook.jp>
2022-06-03 09:13:55 +0900

    update: bindingの追加

--- Client/app/src/main/java/io/github/com/harutiro/tempmanager/WelcomeActivity.kt
+++ Client/app/src/main/java/io/github/com/harutiro/tempmanager/WelcomeActivity.kt
@@ -2,10 +2,20 @@ package io.github.com.harutiro.tempmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.com.harutiro.tempmanager.databinding.ActivityMainBinding
+import io.github.com.harutiro.tempmanager.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
