bcc0139ed578ecb9a9f2fb7043762cc6d7ab3617
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-06-05 02:50:02 +0900

    add:firebase

--- Backend/firebase.py
+++ Backend/firebase.py
@@ -1,10 +1,13 @@
+import datetime
+from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
 import time
 import threading
-import csv
-from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement
+from csv import reader
 
-major_list = []
-uuid_list = []
+uuid = []
+major = []
 
 def worker():
     #print(time.time())
@@ -20,44 +23,112 @@ def schedule(interval, f, wait=True):
     next_time = ((base_time - time.time()) % interval) or interval
     time.sleep(next_time)
 
-while True:
-    def callback(bt_addr, rssi, packet, additional_info):
-        # major_list = []
-        # uuid_list = []
-        major_list.append(packet.major)
-        uuid_list.append(packet.uuid)
-        #f = open('beacon_out.csv', 'w')
-        # data = [packet.major,packet.uuid]
-        # writer = csv.writer(f)
-        # writer.writerow(data)
-        # f.close()
-        #print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+def main():
+    # ===================== Firebase =====================================
+    # このPythonファイルと同じ階層に認証ファイルを配置して、ファイル名を格納
+    JSON_PATH = './geekcamp.json'
 
-# scan for all iBeacon advertisements from beacons with certain properties:
-# - uuid
-# - major
-# - minor
-# at least one must be specified.
+    # Firebase初期化
+    cred = credentials.Certificate(JSON_PATH)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    #name = np.array(['hoge','fuga','piyo'],dtype=str)
 
-    scanner = BeaconScanner(callback, device_filter=IBeaconFilter(uuid="e5b9e3a6-27e2-4c36-a257-7698da5fc140")
-    )
-    scanner.start()
-    time.sleep(5)
-    scanner.stop()
+    doc_user = db.collection('User').get()
+    # ====================================================================
 
-# scan for all iBeacon advertisements regardless from which beacon
-    scanner = BeaconScanner(callback,
-        packet_filter=IBeaconAdvertisement
-    )
-    scanner.start()
-    time.sleep(5)
-    scanner.stop()
-    print(major_list,uuid_list)
-    f = open('beacon_out.csv', 'w')
-    data = [major_list,uuid_list]
-    writer = csv.writer(f,lineterminator='\n')
-    writer.writerow(data)
-    f.close()
-    major_list = []
-    uuid_list = []
-    schedule(5, worker)
+
+    while True:
+        with open('./beacon_out.csv', 'r') as csv_file:
+            csv_reader = reader(csv_file)
+    # Passing the cav_reader object to list() to get a list of lists
+            major = []
+            uuid = []
+            for row in csv_reader:
+                major.append(str(row[0]))
+                uuid.append(str(row[1]))
+            #print(list_of_rows[0][0])
+            print(major)
+            #print(list_of_rows[0][1])
+        dt_now = datetime.datetime.now()
+        try:
+            # Firestoreのコレクションにアクセス
+            doc_ref = db.collection(u'RoomLog').document(dt_now.strftime(u'%Y-%m-%d')).collection(u"Times").document(dt_now.strftime(u'%H:%M'))
+            # Firestoreにドキュメントidを指定しないで１つづつニュースを保存
+            doc_ref.set({
+                u'NAME': 'hoge',
+                u'TIME':dt_now.strftime(u'%H:%M'),
+            })
+        except:
+            print('error')
+        #print(dt_now.strftime(u'%Y-%m-%d %H:%M:%S'))
+        for _ in doc_user:
+            #print(_.id)
+            doc = db.collection(u'User').document(_.id)
+            my_dict = doc.get().to_dict()
+            # print(my_dict["UUID"])
+            # print(my_dict["NAME"])
+        schedule(5, worker)
+print('done')
+
+
+if __name__ == '__main__':
+    main()
+
+# roomlog_name = []
+# dt_now = datetime.datetime.now()
+# tmp_beacon_major = ["566656", "56562","2","56562"]
+# tmp_beacon_uuid = ["ba4d8ef7-51a3-110c-a55d-bca1afbba494","ba4d8ef7-51a3-110c-a55d-bca1afbba495"]
+# tmp2_beacon_uuid = ["ba4d8ef7-51a3-110c-a55d-bca1afbba494","ba4d8ef7-51a3-110c-a55d-bca1afbba496"]
+# tmp_beacon_name = ["mikan","tako","makino"]
+# def main():
+#     # ===================== Firebase =====================================
+#     # このPythonファイルと同じ階層に認証ファイルを配置して、ファイル名を格納
+#     JSON_PATH = './geekcamp.json'
+
+#     # Firebase初期化
+#     cred = credentials.Certificate(JSON_PATH)
+#     firebase_admin.initialize_app(cred)
+#     db = firestore.client()
+#     docs_user = db.collection('User').get()
+#     # ====================================================================
+
+#     # ====================== Beacon ======================================
+#     while True:
+#         # if beacon.packet.major == '56562':
+#         for _ in tmp_beacon_major:
+#             if _ == '56562':
+#                 #for doc in docs_user:
+#                 #if beacon.packet.uuid == doc.UUID:
+#                 if tmp_beacon_uuid in tmp2_beacon_uuid:
+#                     # roomlog_name.append(doc.NAME)
+#                     roomlog_name.append(tmp_beacon_name[_])
+#             # Firestoreのコレクションにアクセス
+#                 try:
+#                     doc_ref = db.collection('TempLog')
+#                 # Firesoreにデータを追加
+#                     doc_ref.add({
+#                         'NAME': roomlog_name,
+#                         'UUID': tmp_beacon_uuid,
+#                         'TIMES': dt_now,
+#                     })
+#                 except:
+#                     print('error')
+#         # for news in res:
+#         #     try:
+#         #         # Firestoreのコレクションにアクセス
+#         #         doc_ref = db.collection('')
+#         #         # Firestoreにドキュメントidを指定しないで１つづつニュースを保存
+#         #         doc_ref.add({
+#         #             'title': news.text,
+#         #             'url': news.attrs['href'],
+#         #             'date': dt_now.strftime('%Y年%m月%d日'),
+#         #         })
+#         #     except:
+#         #         print('error')
+#         time.sleep(10)
+#         print('done')
+#     # ====================================================================
+
+# if __name__ == '__main__':
+#     main()
