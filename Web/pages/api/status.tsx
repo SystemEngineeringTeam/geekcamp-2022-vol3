2b5ffc8110c52df7afb0eb91bc4082885ff1a1bf
hihumikan <musecharo@gmail.com>
2022-06-04 11:53:17 +0900

    add: ?

--- Web/pages/api/status.tsx
+++ Web/pages/api/status.tsx
@@ -14,7 +14,7 @@ function getDate() {
 const cert = {
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 // ステータスを取得する関数
