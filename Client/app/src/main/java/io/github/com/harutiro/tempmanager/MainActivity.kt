f395d8861dfcd1cd08a9e81b9a4fd5e2349e6886
harutiro <harutiro2027@outlook.jp>
2022-06-17 14:58:10 +0900

    update: パーミッションがないと動かないように変更

--- Client/app/src/main/java/io/github/com/harutiro/tempmanager/MainActivity.kt
+++ Client/app/src/main/java/io/github/com/harutiro/tempmanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.com.harutiro.tempmanager
 
+import android.Manifest
 import android.app.*
 import android.content.Context
 import android.content.Intent
@@ -19,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth
 import io.github.com.harutiro.tempmanager.databinding.ActivityMainBinding
 import io.github.com.harutiro.tempmanager.service.IbeaconOutputService
 import org.altbeacon.beacon.*
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity(), RangeNotifier,MonitorNotifier {
 
@@ -33,6 +35,13 @@ class MainActivity : AppCompatActivity(), RangeNotifier,MonitorNotifier {
     var insideRegion = false
 
     private val IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_SCAN,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,69 +78,73 @@ class MainActivity : AppCompatActivity(), RangeNotifier,MonitorNotifier {
 
 
         //ここからビーコン関係
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            val beaconManager = BeaconManager.getInstanceForApplication(this)
 
-        val beaconManager = BeaconManager.getInstanceForApplication(this)
+            // 初期化。これがないと画面を２回以上開いた時に２重でデータを受信してしまう
+            beaconManager.removeAllMonitorNotifiers()
+            beaconManager.removeAllRangeNotifiers()
+            beaconManager.rangedRegions.forEach {region ->
+                beaconManager.stopRangingBeacons(region)
+                beaconManager.stopMonitoring(region)
 
-        // 初期化。これがないと画面を２回以上開いた時に２重でデータを受信してしまう
-        beaconManager.removeAllMonitorNotifiers()
-        beaconManager.removeAllRangeNotifiers()
-        beaconManager.rangedRegions.forEach {region ->
-            beaconManager.stopRangingBeacons(region)
-            beaconManager.stopMonitoring(region)
-
-        }
-
-
-        beaconManager.beaconParsers.clear()
-        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
+            }
 
 
-        // Uncomment the code below to use a foreground service to scan for beacons. This unlocks
-        // the ability to continually scan for long periods of time in the background on Andorid 8+
-        // in exchange for showing an icon at the top of the screen and a always-on notification to
-        // communicate to users that your app is using resources in the background.
-        //
-        val builder = Notification.Builder(this)
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
-        builder.setContentTitle("Scanning for Beacons")
-        val intent = Intent(this, TempEditAcitivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent, PendingIntent.FLAG_MUTABLE
-        )
-        builder.setContentIntent(pendingIntent)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "My Notification Channel ID",
-                "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT
+            beaconManager.beaconParsers.clear()
+            beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
+
+
+            // Uncomment the code below to use a foreground service to scan for beacons. This unlocks
+            // the ability to continually scan for long periods of time in the background on Andorid 8+
+            // in exchange for showing an icon at the top of the screen and a always-on notification to
+            // communicate to users that your app is using resources in the background.
+            //
+            val builder = Notification.Builder(this)
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+            builder.setContentTitle("Scanning for Beacons")
+            val intent = Intent(this, TempEditAcitivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_MUTABLE
             )
-            channel.description = "My Notification Channel Description"
-            val notificationManager = getSystemService(
-                NOTIFICATION_SERVICE
-            ) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-            builder.setChannelId(channel.id)
+            builder.setContentIntent(pendingIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    "My Notification Channel ID",
+                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT
+                )
+                channel.description = "My Notification Channel Description"
+                val notificationManager = getSystemService(
+                    NOTIFICATION_SERVICE
+                ) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+                builder.setChannelId(channel.id)
+            }
+            beaconManager.enableForegroundServiceScanning(builder.build(), 456)
+
+            beaconManager.setEnableScheduledScanJobs(false)
+            beaconManager.backgroundBetweenScanPeriod = 0
+            beaconManager.backgroundScanPeriod = 1100
+
+            Log.d(TAG, "setting up background monitoring in app onCreate")
+            beaconManager.addMonitorNotifier(this)
+
+
+            // If we were monitoring *different* regions on the last run of this app, they will be
+            // remembered.  In this case we need to disable them here
+            for (region in beaconManager.monitoredRegions) {
+                beaconManager.stopMonitoring(region!!)
+            }
+
+            beaconManager.startMonitoring(region)
+
+            beaconManager.addRangeNotifier(this)
+
+            beaconManager.startRangingBeacons(region)
         }
-        beaconManager.enableForegroundServiceScanning(builder.build(), 456)
-
-        beaconManager.setEnableScheduledScanJobs(false)
-        beaconManager.backgroundBetweenScanPeriod = 0
-        beaconManager.backgroundScanPeriod = 1100
-
-        Log.d(TAG, "setting up background monitoring in app onCreate")
-        beaconManager.addMonitorNotifier(this)
 
 
-        // If we were monitoring *different* regions on the last run of this app, they will be
-        // remembered.  In this case we need to disable them here
-        for (region in beaconManager.monitoredRegions) {
-            beaconManager.stopMonitoring(region!!)
-        }
 
-        beaconManager.startMonitoring(region)
-
-        beaconManager.addRangeNotifier(this)
-
-        beaconManager.startRangingBeacons(region)
 
 
 
